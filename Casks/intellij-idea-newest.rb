8e8c657665cf42fc88525005be842e68e136fab9
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-05-16 17:27:34 +0200

    Update to new EAP version for 2018.2

--- Casks/intellij-idea-newest.rb
+++ Casks/intellij-idea-newest.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-newest' do
-  version '2018.1,181.4445.20'
-  sha256 'effe54ed241c9a608df21cac1bbdffa5a9c0b486432d5694f97248e6760331ba'
+  version '2018.2,182.2371.4'
+  sha256 '33baf59688ea2f3c83e1efbaf904228d7d33053f815b36e2048985dd62f356aa'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=eap',
@@ -10,8 +10,8 @@ cask 'intellij-idea-newest' do
 
   auto_updates true
 
-  #app "IntelliJ IDEA #{version.major_minor} EAP.app"
-  app "IntelliJ IDEA.app"
+  app "IntelliJ IDEA #{version.major_minor} EAP.app"
+  #app "IntelliJ IDEA.app"
 
   postflight do
     full_path = "#{ENV['HOME']}/Library/Preferences/IntelliJIdea#{version.major_minor}"
