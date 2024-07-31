2d8d49b3c471443b140432c20bd442e5f38c8c53
Stig Kleppe-Jørgensen <14671+stigkj@users.noreply.github.com>
2024-07-31 15:45:24 +0200

    Upgrade IntelliJ to v242.20224.159
    This is the 2024.2 release candidate, so the app name does not have the
    EAP part.

--- Casks/intellij-idea-newest.rb
+++ Casks/intellij-idea-newest.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-newest' do
-  version '2024.2,242.14146.16'
-  sha256 '70897070443a1abe30aafa58b726e98030185cb632d1583fa86f3e1e3daa0a23'
+  version '2024.2,242.20224.159'
+  sha256 '2e401544561513a4ad272b5ce08748f73a1d63ac5ab9b75b65702f643209d8c5'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}-aarch64.dmg"
   #url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}-aarch64.dmg"
@@ -9,8 +9,8 @@ cask 'intellij-idea-newest' do
 
   auto_updates true
   
-  app "IntelliJ IDEA #{version.major_minor} EAP.app"
-  #app "IntelliJ IDEA.app"
+  #app "IntelliJ IDEA #{version.major_minor} EAP.app"
+  app "IntelliJ IDEA.app"
 
   postflight do
     full_path = "#{ENV['HOME']}/Library/Application Support/JetBrains/IntelliJIdea#{version.major_minor}"
